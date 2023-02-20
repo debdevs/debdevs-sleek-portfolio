c0d667b667e3b8c032457ad4086afd1b1967aec1
debdevs <debdevs97@gmail.com>
2023-02-20 00:44:53 -0500

    add fonts and create HomeHero base code

---
+++ debdevs-sleek-portfolio-site/src/components/templates/HomeHero/HomeHero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomeHero.css'
+const HomeHero = () => {
+  return (
+    <div className='home_hero_section'>
+       <div className='home_hero_contents'>
+        
+       </div>
+    </div>
+  )
+}
+
+export default HomeHero
