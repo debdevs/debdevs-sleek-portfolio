081ef26583e93352e9330b9e09e7193b5ecb6103
Deborah <debdevs97@gmail.com>
2023-11-10 12:47:26 -0500

    update element sizing and display for responsive
    update element sizing and display for responsiveness

--- debdevs-sleek-portfolio-site/src/components/UI/atoms/BylineText/BylineText.jsx
+++ debdevs-sleek-portfolio-site/src/components/UI/atoms/BylineText/BylineText.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./BylineText.css"
-const BylineText = ({text = "Frontend Developer ● Animator ● Visual designer"}) => {
+const BylineText = ({text = "UI/UX Engineer ● Animator ● Visual designer"}) => {
   return (
     <p className='byline_text'>{text}</p>
   )
