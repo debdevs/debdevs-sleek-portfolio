5907f07f437f63437ccdfeaf804162691f00f895
Deborah <debdevs97@gmail.com>
2023-11-25 21:49:55 -0500

    create PDF download functionality
    has errors

--- debdevs-sleek-portfolio-site/src/components/UI/molecules/MainInfoContainer/MainInfoContainer.jsx
+++ debdevs-sleek-portfolio-site/src/components/UI/molecules/MainInfoContainer/MainInfoContainer.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import './MainInfoContainer.css'
-import TextsInfoBox from '../../atoms/TextsInfoBox/TextsInfoBox'
-import SecondaryThemeButton from '../../atoms/SecondaryThemeButton/SecondaryThemeButton'
-import TechStackArray from '../../atoms/TechStackArray/TechStackArray'
-import TertiaryThemeButton from '../../atoms/TertiaryThemeButton/TertiaryThemeButton'
-import { BsHdd, BsHddNetwork } from 'react-icons/bs'
-const MainInfoContainer = ({image_data}) => {
+import React from "react";
+import "./MainInfoContainer.css";
+import TextsInfoBox from "../../atoms/TextsInfoBox/TextsInfoBox";
+import SecondaryThemeButton from "../../atoms/SecondaryThemeButton/SecondaryThemeButton";
+import TechStackArray from "../../atoms/TechStackArray/TechStackArray";
+import TertiaryThemeButton from "../../atoms/TertiaryThemeButton/TertiaryThemeButton";
+import { BsHdd, BsHddNetwork } from "react-icons/bs";
+
+const MainInfoContainer = ({ image_data }) => {
   return (
-    <div className='main_info_container_parent'>
-        <TextsInfoBox/>
-        <div className='info_tech_items'>
-        <h3 className='project_display_accent_header'>Skills:</h3>
-        <TechStackArray image = {image_data}/>
-        </div>
-       <div className='main_info_button_rows'>
+    <div className="main_info_container_parent">
+      <TextsInfoBox />
+      <div className="info_tech_items">
+        <h3 className="project_display_accent_header">Skills:</h3>
+        <TechStackArray image={image_data} />
+      </div>
+      <div className="main_info_button_rows">
+        <a
+          href="src/assets/documents/Deborah_Daniels_Resume.pdf"
+          download="Deborah_Daniels_Resume.pdf"
+        >
+          <SecondaryThemeButton />
+        </a>
 
-       <SecondaryThemeButton/>
-        <TertiaryThemeButton text='Work' icon ={<BsHddNetwork/>} />
-
-       </div>
-
-
-
-
-       
+        <TertiaryThemeButton text="Work" icon={<BsHddNetwork />} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainInfoContainer
+export default MainInfoContainer;
