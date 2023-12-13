7a5e19d7e407d4820b0539ad64a8e39b8c6099da
Deborah <debdevs97@gmail.com>
2023-12-13 11:52:37 -0500

    edit byline text

--- debdevs-sleek-portfolio-site/src/components/UI/atoms/BylineText/BylineText.jsx
+++ debdevs-sleek-portfolio-site/src/components/UI/atoms/BylineText/BylineText.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./BylineText.css"
-const BylineText = ({text = "UI/UX Engineer ● Animator ● Visual designer"}) => {
+const BylineText = ({text = "UI/UX Engineer ● Animator ● Visual Designer"}) => {
   return (
     <p className='byline_text'>{text}</p>
   )
